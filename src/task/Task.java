74c7ce88a772bcb88d573de77c70a072517bee4c
Zaza <ilyatyuleneff@yandex.ru>
2025-07-17 15:15:34 +0300

    Исправлены ошибки с CheckStyle в Task

--- src/task/Task.java
+++ src/task/Task.java
@@ -77,7 +77,7 @@ public class Task {
     }
 
     public LocalDateTime getEndTime() {
-        if(startTime == null || duration == null) {
+        if (startTime == null || duration == null) {
             return null;
         }
         return startTime.plus(duration);
