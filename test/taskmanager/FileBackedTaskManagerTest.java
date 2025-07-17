603826f350a267dfd4542077a7e588eff9a08df6
Zaza <ilyatyuleneff@yandex.ru>
2025-07-17 15:22:32 +0300

    Исправлены ошибка в фиксированном име файла FileBackedTaskManagerTest

--- test/taskmanager/FileBackedTaskManagerTest.java
+++ test/taskmanager/FileBackedTaskManagerTest.java
@@ -1,10 +1,16 @@
 package taskmanager;
 
 import java.io.File;
+import java.io.IOException;
 
 class FileBackedTaskManagerTest extends TaskManagerTest<FileBackedTaskManager> {
     @Override
     FileBackedTaskManager createManager() {
-        return FileBackedTaskManager.loadFromFile(new File("test.csv"));
+        try {
+            File file = File.createTempFile("test", ".csv");
+            return FileBackedTaskManager.loadFromFile(file);
+        } catch (IOException e) {
+            throw new RuntimeException("Не удалось создать временный файл для теста", e);
+        }
     }
 }
